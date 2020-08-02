ac6042bd0797ccd7e8f6dfc950023072def89f66
Sebastian Ignacio Acevedo <zebas_87@hotmail.com>
2020-08-01 22:54:08 -0300

    Initial Commit

---
+++ Assets/Script/Final Movement/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float playerSpeed = 10f;
+    [SerializeField] float gravity = -9.81f;
+    CharacterController controller;
+    Vector3 velocity;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.5f;
+    [SerializeField] LayerMask groundMask;
+    bool isGrounded;
+
+    [SerializeField] float jumpHeight;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        // We make our player movement according his Vector3 position
+        Vector3 direction = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(direction * playerSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Here i apply gravity to the player.
+        velocity.y += gravity * Time.deltaTime;        
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    
+}
---
+++ Assets/Script/Movement A/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float speed = 5f;
+    [SerializeField] float gravity = 9.81f;
+    
+    CharacterController controller;
+   
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        
+    }
+
+    
+    void Update()
+    {
+        PlayerMovement();
+        
+    }
+
+    private void PlayerMovement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(moveX, 0f, moveZ); // We add the direction the Player should move
+        Vector3 velocity = direction * speed; // Here we store the direction of the movement with the velocity
+
+        velocity.y -= gravity * Time.deltaTime; // I try to keep it consistent for my physics based characther.
+        velocity = transform.transform.TransformDirection(velocity);
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+}
