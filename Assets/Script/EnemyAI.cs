4ebbc81c118bb6abe583954a8f7bdc589ee111a1
Sebastian Ignacio Acevedo <zebas_87@hotmail.com>
2020-08-03 00:22:57 -0300

    Enemy AI update and Enemy Prefab

--- Assets/Script/EnemyAI.cs
+++ Assets/Script/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,15 +7,58 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    bool isProvoked;
+    
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        // We check if the enemy is provoked by shooting or hiting him.
+        if (isProvoked)
+        {
+            EngageTarget();           
+        }
+        // We check the distance betwen the target and the enemy, if in range the enemy chase you.
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;            
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+
+    private void EngageTarget()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
         navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("I am currently attacking you");
     }
 }
